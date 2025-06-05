282066458d35ee530a2bfca2369db6705eea0f25
ianlee23 <leeianchristopher@gmail.com>
2025-06-05 14:24:48 +0800

    change2

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const markCallReadRouter = require('./routes/mark-call-read');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
-
-// Prefix the routes from mark-call-read.js with /api/mark-call-read
 app.use('/api/mark-call-read', markCallReadRouter);
 
 app.get('/', (req, res) => {
